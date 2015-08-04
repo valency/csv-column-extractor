f872df86bf18b0437dd86b6f023711974c52bce5
Ye Ding <guiewy@gmail.com>
2015-08-04 20:29:23 +0800

    v1.1.23

--- CSV Column Extractor/Form1.Designer.cs
+++ CSV Column Extractor/Form1.Designer.cs
@@ -24,23 +24,15 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.colno = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.srcpath = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.colno)).BeginInit();
+            this.colval = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // colno
-            // 
-            this.colno.Location = new System.Drawing.Point(8, 40);
-            this.colno.Name = "colno";
-            this.colno.Size = new System.Drawing.Size(56, 23);
-            this.colno.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 40);
+            this.button1.Location = new System.Drawing.Point(72, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(184, 24);
             this.button1.TabIndex = 2;
@@ -61,19 +53,26 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // colval
+            // 
+            this.colval.Location = new System.Drawing.Point(8, 37);
+            this.colval.Name = "colval";
+            this.colval.Size = new System.Drawing.Size(58, 23);
+            this.colval.TabIndex = 4;
+            this.colval.Text = "0,1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(265, 73);
+            this.Controls.Add(this.colval);
             this.Controls.Add(this.srcpath);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.colno);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "CSV Column Extractor";
-            ((System.ComponentModel.ISupportInitialize)(this.colno)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,10 +80,10 @@
 
         #endregion
 
-        private System.Windows.Forms.NumericUpDown colno;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox srcpath;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox colval;
     }
 }
 
--- CSV Column Extractor/Form1.cs
+++ CSV Column Extractor/Form1.cs
@@ -21,15 +21,23 @@ namespace CSV_Column_Extractor {
 
         private void button1_Click(object sender, EventArgs e) {
             button1.Enabled = false;
-            int column = (int)colno.Value;
+            String[] colstr = colval.Text.Split(',');
+            int[] column = new int[colstr.Length];
+            for (int i = 0; i < column.Length; i++) {
+                column[i] = int.Parse(colstr[i]);
+            }
             StreamReader reader = new StreamReader(srcpath.Text);
-            StreamWriter writer = new StreamWriter(srcpath.Text + "_COL_" + column + ".txt");
+            StreamWriter writer = new StreamWriter(srcpath.Text + ".extract");
             string s = reader.ReadLine();
             int count = 0;
             int prevsec = DateTime.Now.Second;
             while (s != null) {
                 string[] ss = s.Split(',');
-                writer.WriteLine(ss[column]);
+                for (int i = 0; i < column.Length; i++) {
+                    writer.Write(ss[column[i]]);
+                    if (i < column.Length - 1) writer.Write(",");
+                    else writer.WriteLine();
+                }
                 // Move to next
                 s = reader.ReadLine();
                 count++;
@@ -42,6 +50,7 @@ namespace CSV_Column_Extractor {
             reader.Close();
             writer.Close();
             button1.Text = "FINISHED: " + count;
+            button1.Enabled = true;
             Application.DoEvents();
         }
     }
